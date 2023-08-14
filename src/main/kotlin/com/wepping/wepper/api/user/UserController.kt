1c29394751b5174bbd4fed0cef4492dde156c021
ladianchad <qhrejddlvltm@gmail.com>
2023-08-14 09:19:43 +0900

    add version
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- src/main/kotlin/com/wepping/wepper/api/user/UserController.kt
+++ src/main/kotlin/com/wepping/wepper/api/user/UserController.kt
@@ -3,20 +3,19 @@ package com.wepping.wepper.api.user
 import com.wepping.wepper.`interface`.user.dto.UserDto
 import com.wepping.wepper.`interface`.user.service.UserService
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class UserController (
-    private val userService: UserService
+class UserController(
+    private val userService: UserService,
 ) {
 
-    @GetMapping("/users")
+    @GetMapping("/v1/users")
     fun getUsers(): List<UserDto> {
         return this.userService.getAllUsers()
     }
 
-    @GetMapping("/users/:id")
+    @GetMapping("/v1/users/:id")
     fun getUserById(id: Long): UserDto {
         return this.userService.getUserById(id)
     }
