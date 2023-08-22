00e369681bc7b1ba83ea3a367d719dc900fea4bd
ladianchad <qhrejddlvltm@gmail.com>
2023-08-22 22:28:51 +0900

    move class to inner class
    Signed-off-by: ladianchad <qhrejddlvltm@gmail.com>

--- src/main/kotlin/com/wepping/wepper/common/exception/HttpException.kt
+++ src/main/kotlin/com/wepping/wepper/common/exception/HttpException.kt
@@ -1,19 +1,20 @@
 package com.wepping.wepper.common.exception
 
-enum class HttpErrorCode(val code: Int) {
-    BAD_REQUEST(400),
-    UNAUTHORIZED(401),
-    FORBIDDEN(403),
-    NOT_FOUND(404),
-    CONFLICT(409),
-    GONE(410),
-    INTERNAL_SERVER_ERROR(500),
-    SERVICE_UNAVAILABLE(503)
+open class HttpException(
+    errorCode: ErrorCode,
+    message: String
+) : BaseException(errorCode, message) {
+
+    enum class ErrorCode(val code: Int) {
+        BAD_REQUEST(400),
+        UNAUTHORIZED(401),
+        FORBIDDEN(403),
+        NOT_FOUND(404),
+        CONFLICT(409),
+        GONE(410),
+        INTERNAL_SERVER_ERROR(500),
+        SERVICE_UNAVAILABLE(503)
+    }
 }
 
-open class HttpException(
-    errorCode: HttpErrorCode,
-    message: String
-) : BaseException(errorCode, message)
-
-class NotFoundException(message: String) : HttpException(HttpErrorCode.NOT_FOUND, message)
+class NotFoundException(message: String) : HttpException(ErrorCode.NOT_FOUND, message)
